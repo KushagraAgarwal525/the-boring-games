b98c4d2070e2244f1b0b676207d0f4c95fc7f127
Kushagra Agarwal <kushagraagarwal525.ka@gmail.com>
2022-07-12 14:45:38 +0300

    Complete 1% and add help widget icom

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import gameReducer from "./reducers/gameReducer";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import gameReducer from './reducers/gameReducer';
 // import reportWebVitals from './reportWebVitals';
 
 const store = configureStore({
-  reducer: {
-    game: gameReducer,
-  },
+	reducer: {
+		game: gameReducer,
+	},
 });
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Provider store={store}>
+		<App />
+	</Provider>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// reportWebVitals(console.log);
